fd36b051c26e240375c55ad984a1a6b1727c60f1
jcaiqueoliveira <j.caique_fla@hotmail.com>
2019-06-18 23:43:16 -0300

    add debug variant

--- app/src/main/java/gdg/aracaju/data/api/ApiBuilder.kt
+++ app/src/main/java/gdg/aracaju/data/api/ApiBuilder.kt
@@ -11,31 +11,27 @@ import gdg.aracaju.data.api.extensions.readList
 import gdg.aracaju.data.api.extensions.readValue
 import gdg.aracaju.data.api.extensions.signIn
 import gdg.aracaju.domain.model.LoginState
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.withTimeout
-import kotlin.coroutines.CoroutineContext
 
-internal object ApiBuilder : ServerGateway, CoroutineScope {
-
-    override val coroutineContext: CoroutineContext
-        get() = IO
+internal object ApiBuilder : ServerGateway {
 
     private const val TIMEOUT = 10_000L
 
     private val instance by lazy { FirebaseDatabase.getInstance() }
     private val auth by lazy { FirebaseAuth.getInstance() }
 
-    override suspend fun fetchEvents(): List<EventsResponse> {
-        return withTimeout(TIMEOUT) {
-            instance.getReference(EVENT).orderByKey().readList<EventsResponse>()
-        }
+    override suspend fun fetchEvents(): List<EventsResponse> = withTimeout(TIMEOUT) {
+        instance
+            .getReference(EVENT)
+            .orderByKey()
+            .readList<EventsResponse>()
     }
 
-    override suspend fun fetchDetails(id: Int): DetailResponse {
-        return withTimeout(TIMEOUT) {
-            instance.getReference(DETAIL).child(id.toString()).readValue<DetailResponse>()
-        }
+    override suspend fun fetchDetails(id: Int): DetailResponse = withTimeout(TIMEOUT) {
+        instance
+            .getReference(DETAIL)
+            .child(id.toString())
+            .readValue<DetailResponse>()
     }
 
     override suspend fun authenticate(token: String?): LoginState {
@@ -43,9 +39,7 @@ internal object ApiBuilder : ServerGateway, CoroutineScope {
         return auth.signIn(credential)
     }
 
-    override fun logout() {
-        auth.signOut()
-    }
+    override fun logout() = auth.signOut()
 
     override fun hasUser(): Boolean = auth.currentUser != null
 }
